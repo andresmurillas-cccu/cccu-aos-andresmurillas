e76c8f50e1197ab36e274fbc222145e7034f870b
Andres Murillas <am2208@canterbury.ac.uk>
2023-05-18 16:39:27 +0200

    manguo

--- src/email.py
+++ src/email.py
@@ -1,6 +1,18 @@
-import 
+import exchangelib
 
 class Emails:
     
+    email = ""
+    username = ""
+    password = ""
+    
+    
     def __init__(self):
         pass
+    
+    def setup(self):
+        self.email = input("email: ")
+        self.username = input("user name: ")
+        self.password = input("password: ")
+
+    def 
